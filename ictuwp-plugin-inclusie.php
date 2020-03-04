cc5aeada1e26a51375f813c60153ff3b5b9ad8f0
Paul van Buuren <paul@wbvb.nl>
2020-03-04 20:43:21 +0100

    1.1.7 - Overbodige layout in zoekresultaten weggehaald.

--- ictuwp-plugin-inclusie.php
+++ ictuwp-plugin-inclusie.php
@@ -8,8 +8,8 @@
     * Plugin Name:            ICTU / Gebruiker Centraal / Inclusie post types and taxonomies
     * Plugin URI:             https://github.com/ICTU/Gebruiker-Centraal---Inclusie---custom-post-types-taxonomies
     * Description:            Plugin for digitaleoverheid.nl to register custom post types and custom taxonomies 
-    * Version:                1.1.6
-    * Version description:    Functions from new version of GC-theme. Further integration.
+    * Version:                1.1.7
+    * Version description:    Overbodige layout in zoekresultaten weggehaald.
     * Author:                 Tamara de Haas & Paul van Buuren
     * Author URI:             https://wbvb.nl/
     * License:                GPL-2.0+
@@ -88,7 +88,7 @@ if (!defined('ICTU_GC_CPT_PROCESTIP')) {
 define('ICTU_GC_ARCHIVE_CSS', 'ictu-gc-header-css');
 define('ICTU_GC_BASE_URL', trailingslashit(plugin_dir_url(__FILE__)));
 define('ICTU_GC_ASSETS_URL', trailingslashit(ICTU_GC_BASE_URL));
-define('ICTU_GC_INCL_VERSION', '1.1.6');
+define('ICTU_GC_INCL_VERSION', '1.1.7');
 
 //========================================================================================================
 
@@ -411,6 +411,11 @@ if (!class_exists('ICTU_GC_Register_taxonomies')) :
             if (!$homepageID) {
                 return;
             }
+            if ( is_search() ) {
+	            // geen stappen tonen als deze pagina getoond wordt in sitesearch zoekresultaten
+				// @since	1.1.7
+                return;
+            }
 
             if (function_exists('get_field')) {
 
@@ -477,6 +482,12 @@ if (!class_exists('ICTU_GC_Register_taxonomies')) :
         public function ictu_gc_frontend_stap_inleiding() {
 
             global $post;
+
+            if ( is_search() ) {
+	            // geen inleiding tonen als deze pagina getoond wordt in sitesearch zoekresultaten
+				// @since	1.1.7
+                return;
+            }
 
             if (function_exists('get_field')) {
 
